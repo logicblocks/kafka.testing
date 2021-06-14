620fae90df68ecb534e3aba7fb0e9ed678586faf
Circle CI <circleci@infrablocks.io>
2021-06-14 12:27:14 +0000

    Pre-release version 0.0.1-RC3 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/kafka.testing "0.0.1-RC2"
+(defproject io.logicblocks/kafka.testing "0.0.1-RC3"
   :description "A Clojure library to help with tests involving Kafka."
   :url "https://github.com/logicblocks/kafka.testing"
 
