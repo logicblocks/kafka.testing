115dde97b93c94c1f299f9f567622bef194d36bc
Toby Clemson <tobyclemson@gmail.com>
2021-06-15 14:39:12 +0100

    Remove unused core namespace.

--- src/kafka/testing/core.clj
+++
@@ -1,1 +0,0 @@
-(ns kafka.testing.core)
--- test/kafka/testing/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns kafka.testing.core-test
-  (:require
-   [clojure.test :refer :all]
-
-   [kafka.testing.logging]))
-
-(deftest does-something
-  (is (= 1 1)))
